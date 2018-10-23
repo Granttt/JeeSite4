c834926db858a80ef17da2b0887f90847e78b29b
gaoxiyang <gaoxiyang1237@126.com>
2018-10-23 12:37:11 +0800

    修改了图片存储路径及后端基础路径adminPath002

--- web/src/main/java/com/jeesite/modules/Application.java
+++ web/src/main/java/com/jeesite/modules/Application.java
@@ -14,6 +14,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
  * @version 2018-10-13
  * Github:Granttt
  * 默认最高管理员账号：system  密码：admin
+ * gaoxiyang
  */
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
